bb9e95878c139860baf679512f534077ade85b52
JokerJak <jakmaynard@gmail.com>
2025-10-26 00:59:30 -0600

    ping host loop, ticker doesn't work yet

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+)
 
-func main() {fmt.Println("Hello, World!")}
+var (
+	hosts    *string
+	port     *int
+	interval *int
+	counter  *int
+)
+
+func init() {
+	hosts = flag.String("hosts", "localhost", "Comma-separated list of hostnames")
+	port = flag.Int("port", 80, "Port number")
+	interval = flag.Int("interval", 10, "Interval in seconds")
+}
+
+func main() {
+	flag.Parse()
+	counter = new(int)
+	*counter = 0
+	tk := time.NewTicker(time.Duration(*interval) * time.Second)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		for ; ; <-tk.C {
+			time, err := PingHost(*hosts, *port, *interval)
+			if err != nil {
+				fmt.Fprintf(w, "Error: %v\n", err)
+				return
+			}
+			fmt.Fprintf(w, "Host is reachable. Ping time to %s:%d is %v\n", *hosts, *port, time)
+			fmt.Fprintf(w, "Ping count: %d\n", *counter)
+			fmt.Fprintf(w, "Interval: %d seconds\n", *interval)
+			*counter++
+		}
+	})
+
+	http.ListenAndServe(":80", nil)
+
+}
+
+func PingHost(host string, port int, interval int) (time.Duration, error) {
+	start := time.Now()
+	_, err := net.LookupHost(host)
+	if err != nil {
+		return 0, err
+	}
+	return time.Since(start), nil
+}
